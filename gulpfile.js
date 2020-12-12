d6e29bcfdf7d9e58c1df47f8fa1cf9aaaadc63bc
xpendmusic <xpendmusic@gmail.com>
2020-12-12 16:52:20 -0500

    Actualizacion v1.0.4

--- gulpfile.js
+++ gulpfile.js
@@ -7,30 +7,119 @@ var concat = require('gulp-concat');
 
 sass.compiler = require('node-sass');
 
-gulp.task('task1', function () {
-    return gulp.src('./src/scss/ascend.scss')
+gulp.task('ascendcss', function () {
+    return gulp.src('./src/scss/ascendcss.scss')
         .pipe(sass())
         .pipe(autoprefixer({
             browsers: ['last 2 versions']
         }))
-        .pipe(gulp.dest('./_site/css/'))
         .pipe(gulp.dest('./dist/css/'))
-}
-);
-gulp.task('task2', function () {
-    return gulp.src('./src/scss/ascend.scss')
         .pipe(sass({
             outputStyle: 'compressed'
         }))
+        .pipe(concat('ascendcss.min.css'))
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(gulp.dest('./_site/css/'))
+}
+);
+
+gulp.task('flexbox', function () {
+    return gulp.src('./src/scss/ascendcss-flexbox.scss')
+        .pipe(sass())
         .pipe(autoprefixer({
             browsers: ['last 2 versions']
         }))
-        .pipe(concat('ascend.min.css'))
         .pipe(gulp.dest('./dist/css/'))
-        .pipe(gulp.dest('./_site/css/'));
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(concat('ascendcss-flexbox.min.css'))
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(gulp.dest('./_site/css/'))
 }
 );
-gulp.task('task3', function () {
+
+gulp.task('cssgrid', function () {
+    return gulp.src('./src/scss/ascendcss-cssgrid.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(concat('ascendcss-cssgrid.min.css'))
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(gulp.dest('./_site/css/'))
+}
+);
+
+gulp.task('layout', function () {
+    return gulp.src('./src/scss/ascendcss-layout.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(concat('ascendcss-layout.min.css'))
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(gulp.dest('./_site/css/'))
+}
+);
+
+gulp.task('button', function () {
+    return gulp.src('./src/scss/ascendcss-button.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(concat('ascendcss-button.min.css'))
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(gulp.dest('./_site/css/'))
+}
+);
+
+gulp.task('form', function () {
+    return gulp.src('./src/scss/ascendcss-form.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(concat('ascendcss-form.min.css'))
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(gulp.dest('./_site/css/'))
+}
+);
+
+gulp.task('navbar', function () {
+    return gulp.src('./src/scss/ascendcss-navbar.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(concat('ascendcss-navbar.min.css'))
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(gulp.dest('./_site/css/'))
+}
+);
+
+gulp.task('doc-style', function () {
     return gulp.src('./src/scss/web-main.scss')
         .pipe(sass({
             outputStyle: 'compressed'
@@ -43,6 +132,6 @@ gulp.task('task3', function () {
 }
 );
 
-gulp.task('watch', function () {
-    gulp.watch('./src/scss/**/*.scss', gulp.series('task1', 'task2', 'task3'));
+gulp.task('dev', function () {
+    gulp.watch('./src/scss/**/*.scss', gulp.series('ascendcss', 'flexbox', 'cssgrid', 'layout', 'button', 'form', 'navbar', 'doc-style'));
 });
