a2d88d855f05538e8bf970565840e67b2c6ffc8c
Frank Alvarez <frankcontacto64@gmail.com>
2021-03-03 17:08:30 -0500

    v1.1.4 beta

--- README.md
+++
@@ -1,7 +0,0 @@
-# Ascend
-
-![Logo de Ascend](http://ascendcss.com/static/img/logo.png)
-
-Ascend es una librería de CSS que te permitirá crear layouts para tu sitio web de la forma mas rápida y amigable posible.
-
-Documentación: https://ascendcss.com/documentacion/
--- gulpfile.js
+++ gulpfile.js
@@ -1,137 +1,19 @@
-'use strict';
-
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
 
 sass.compiler = require('node-sass');
 
 gulp.task('ascendcss', function () {
     return gulp.src('./src/scss/ascendcss.scss')
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions']
-        }))
-        .pipe(gulp.dest('./dist/css/'))
         .pipe(sass({
-            outputStyle: 'compressed'
+            outputStyle: 'expanded'
         }))
-        .pipe(concat('ascendcss.min.css'))
-        .pipe(gulp.dest('./dist/css/'))
-        .pipe(gulp.dest('./_site/css/'))
-}
-);
-
-gulp.task('flexbox', function () {
-    return gulp.src('./src/scss/ascendcss-flexbox.scss')
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions']
-        }))
-        .pipe(gulp.dest('./dist/css/'))
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .pipe(concat('ascendcss-flexbox.min.css'))
-        .pipe(gulp.dest('./dist/css/'))
-        .pipe(gulp.dest('./_site/css/'))
-}
-);
-
-gulp.task('cssgrid', function () {
-    return gulp.src('./src/scss/ascendcss-cssgrid.scss')
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions']
-        }))
-        .pipe(gulp.dest('./dist/css/'))
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .pipe(concat('ascendcss-cssgrid.min.css'))
-        .pipe(gulp.dest('./dist/css/'))
-        .pipe(gulp.dest('./_site/css/'))
-}
-);
-
-gulp.task('layout', function () {
-    return gulp.src('./src/scss/ascendcss-layout.scss')
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions']
-        }))
-        .pipe(gulp.dest('./dist/css/'))
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .pipe(concat('ascendcss-layout.min.css'))
-        .pipe(gulp.dest('./dist/css/'))
-        .pipe(gulp.dest('./_site/css/'))
-}
-);
-
-gulp.task('button', function () {
-    return gulp.src('./src/scss/ascendcss-button.scss')
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions']
-        }))
-        .pipe(gulp.dest('./dist/css/'))
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .pipe(concat('ascendcss-button.min.css'))
-        .pipe(gulp.dest('./dist/css/'))
-        .pipe(gulp.dest('./_site/css/'))
-}
-);
-
-gulp.task('form', function () {
-    return gulp.src('./src/scss/ascendcss-form.scss')
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions']
-        }))
-        .pipe(gulp.dest('./dist/css/'))
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .pipe(concat('ascendcss-form.min.css'))
-        .pipe(gulp.dest('./dist/css/'))
-        .pipe(gulp.dest('./_site/css/'))
-}
-);
-
-gulp.task('navbar', function () {
-    return gulp.src('./src/scss/ascendcss-navbar.scss')
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions']
-        }))
-        .pipe(gulp.dest('./dist/css/'))
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .pipe(concat('ascendcss-navbar.min.css'))
-        .pipe(gulp.dest('./dist/css/'))
-        .pipe(gulp.dest('./_site/css/'))
-}
-);
-
-gulp.task('doc-style', function () {
-    return gulp.src('./src/scss/web-main.scss')
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions']
-        }))
-        .pipe(concat('main.min.css'))
-        .pipe(gulp.dest('./_site/css/'));
+        .pipe(concat('test.css'))
+        .pipe(gulp.dest('./dist/test/'))
 }
 );
 
 gulp.task('dev', function () {
-    gulp.watch('./src/scss/**/*.scss', gulp.series('ascendcss', 'flexbox', 'cssgrid', 'layout', 'button', 'form', 'navbar', 'doc-style'));
+    gulp.watch('./src/scss/**/*.scss', gulp.series('ascendcss'));
 });
