e857e1cc3bc5986dfe6f2d648f3f306968694869
iamfrankalvarez <frankcontacto642@gmail.com>
2021-10-02 13:44:59 -0500

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,20 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var stylus = require('gulp-stylus');
-var concat = require('gulp-concat');
-
-sass.compiler = require('node-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const stylus = require('gulp-stylus');
+const rename = require("gulp-rename");
 
 gulp.task('ascendcss', function () {
     return gulp.src('./src/scss/ascendcss.scss')
         .pipe(sass({
             outputStyle: 'expanded'
         }))
-        .pipe(concat('test.css'))
         .pipe(gulp.dest('./dist/test/'))
-}
-);
+});
 
 gulp.task('ascendcss-stylus', function () {
     return gulp.src('./src/stylus/ascendcss.styl')
         .pipe(stylus())
-        .pipe(concat('test-stylus.css'))
+        .pipe(rename('ascendcss-stylus.css'))
         .pipe(gulp.dest('./dist/test/'));
 });
 
